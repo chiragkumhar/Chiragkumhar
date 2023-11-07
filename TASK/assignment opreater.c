28f16bf03ff66de6535c26a445dd8bb8d22a45ae
chiragkumhar <chiragp2704@gmail.com>
2023-11-07 11:42:57 +0530

    Task

---
+++ TASK/assignment opreater.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+	int x=10;
+	x+=5; //x=+5
+	x-=3; //x+x-3
+	x*=50; //x=x*50
+	printf("%d",x);
+}
---
+++ TASK/incrementdecrement.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main()
+{
+	int x=5;
+		// X++; increment
+		x--; //decrement
+		printf("%d",x);
+ }
